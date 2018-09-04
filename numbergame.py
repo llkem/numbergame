7136801c75c9bcaa8a00c636845994cae648cc2a
ryan <inventorryan@gmail.com>
2018-09-04 16:58:33 -0700

    debugging

--- numbergame.py
+++ numbergame.py
@@ -1,14 +1,17 @@
 import random
 
-number = random.randint[0,100]
-guess = raw_input("Guess the number, from one to 100!")
-turns = 0
 
-while guess != number:
-    if guess > number:
-        guess = raw_input("too high, guess again!")
+number = 3 #random.randint(0,100)
+guess = int(input("Guess the number, from one to 100!: "))
+turns = 1
+
+
+while guess != int(number):
+    if int(guess) > int(number):
+        guess = input("too high, guess again!: ")
         turns = turns + 1
-    elif guess < number:
-        guess = raw_input("too low, guess again!")
+    elif int(guess) < int(number):
+        guess = input("too low, guess again!: ")
         turns = turns + 1
-print("good job! the number was %s and you guessed it in %s tries!") %(number,turns)
+    else:
+        print("good job! the number was %s and you guessed it in %s tries!" %(number,turns))
